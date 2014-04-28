e3c3bad7e95310ade94053a488c11910bc979c85
tuomas56 <pigworts2@gmail.com>
2014-04-28 17:36:18 +0100

    fixed up css added responsive image scaling and started on login

--- public_html/js/login.js
+++ public_html/js/login.js
@@ -1,25 +1,24 @@
 $(function () {
     $("#login").submit(function () {
-        var username = $("#username").value;
-        var password = $("#password").value;
-        var salt = makeAjaxRequest("http://marsn.web44.net/login/getsalt.php", "POST", "username=" + username);
-        var hash = CryptoJS.SHA256(password + salt);
-        var success = makeAjaxRequest("http://marsn.web44.net/login/login.php", "POST", "username=" + username + "&password=" + hash);
-        if (success != "true") {
-            alert("Invalid Username or Password");
-        }
+        var username = $("#username").val();
+        var password = $("#password").val();
+        if (username != null && password != null) {
+            $.post("http://marsn.web44.net/getsalt.php", {
+                username: username
+            }, function (data) {
+                //if (salt != "error") {
+                //var hash = CryptoJS.SHA256(password + salt);
+                //var success = makeAjaxRequest("http://marsn.web44.net/login/login.php", "POST", "username=" + username + "&password=" + hash);
+                //if (success != "true") {
+                // alert("Invalid Username or Password");
+                //}
+                //} else {
+                //    alert("Database Error");
+                //}
+                alert(data);
+            });
+        } else
+            alert("You must enter a username and password");
         return false;
     });
 });
-
-function makeAjaxRequest(url, method, params) {
-    var xmlhttp;
-    if (window.XMLHttpRequest) { // code for IE7+, Firefox, Chrome, Opera, Safari
-        xmlhttp = new XMLHttpRequest();
-    } else { // code for IE6, IE5
-        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    xmlhttp.open(method, url, false);
-    xmlhttp.send(params);
-    return xmlhttp.responseText;
-}
