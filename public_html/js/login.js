719b2742ec0ecc9f38f54df3f8f9b171b80372ea
tuomas56 <pigworts2@gmail.com>
2014-04-29 08:40:41 +0100

    added registration

--- public_html/js/login.js
+++ public_html/js/login.js
@@ -30,4 +30,26 @@ $(function () {
             alert("You must enter a username and password");
         return false; //return false to prevent the page from refreshing
     });
+    $("#newuser").submit(function () { //hook function to onsubmit event
+        var username = $("#username").val(); //get username
+        var password = $("#password").val(); //get password
+        if (username != "" && password != "") { //check they arent null
+            $.post("http://marsn.web44.net/getsalt.php", { //make a post to get the salt from database or generate a new one
+                username: username
+            }, function (salt) {
+                if (salt != "error") { //if no error
+                    var pass = password + salt;
+                    var hash = CryptoJS.SHA256(pass) + ''; // make password hash with salt
+                    $.post("http://marsn.web44.net/newuser.php", { //post to login with username and hash
+                        username: username,
+                        password: hash
+                    });
+                } else {
+                    alert("Database Error"); //if error throw database error
+                }
+            });
+        } else
+            alert("You must enter a username and password");
+        return false; //return false to prevent the page from refreshing
+    });
 });
